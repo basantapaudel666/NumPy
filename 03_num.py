Fix variable name from 'average' to 'ave'

03_num.py
@@ -1,4 +1,5 @@
 import numpy as np
 tempreature = np.array([32.5, 23.5, 46.2,34.3, 34, 35, 38, 38.56, 40.23])
-average = np.mean(tempreature)
-print(average)
+ave = np.mean(tempreature)
+print(ave)
+