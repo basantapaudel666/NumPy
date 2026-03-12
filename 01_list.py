00662fe95ef5ff265c7c440d66fd4880a8043a15
Basanta Paudel <basantapaudel997@gmail.com>
2026-03-12 21:20:50 +0545

    Add files via upload

---
+++ 01_list.py
@@ -0,0 +1,6 @@
+Salary=[25000,26000,27000,28000]
+total = 0
+for temp in Salary:
+    total+=temp
+average = total/len(Salary)
+print(average)    
---
+++ 03_num.py
@@ -0,0 +1,4 @@
+import numpy as np
+tempreature = np.array([32.5, 23.5, 46.2,34.3, 34, 35, 38, 38.56, 40.23])
+average = np.mean(tempreature)
+print(average)
