4436b5667193c1546cbb349ab81140982e89c2b7
Basanta Paudel <basantapaudel997@gmail.com>
2026-03-15 22:15:51 +0545

    Add files via upload

--- 03_num.py
+++ 03_num.py
@@ -1,5 +1,4 @@
 import numpy as np
 tempreature = np.array([32.5, 23.5, 46.2,34.3, 34, 35, 38, 38.56, 40.23])
-ave = np.mean(tempreature)
-print(ave)
-
+average = np.mean(tempreature)
+print(average)
---
+++ 10.py
@@ -0,0 +1,3 @@
+import numpy as np
+something = np.array([2,2,2,2,2,2,2,2,2,2,2,2,2,2,2])
+print(something)
